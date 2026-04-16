434ad35de8ac1f74220aed00a9f458779a1bbc9e
Rahim10020 <rahialighi@gmail.com>
2026-04-16 13:04:19 +0000

    modif footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,10 @@ import { APP_URL, WEB_URL } from "@/lib/content";
 
 export default function Footer() {
   return (
-    <footer className="relative overflow-hidden bg-footer-white text-black">
+    <footer className="relative overflow-hidden bg-footer-white text-white">
       <div className="relative mx-auto max-w-7xl px-6 pb-14 pt-16 lg:px-8">
         {/* CTA Card */}
-        <div className="mx-auto max-w-6xl rounded-32 bg-primary px-6 py-10 sm:px-8 sm:py-12">
+        <div className="mx-auto max-w-6xl rounded-32 bg-black px-6 py-10 sm:px-8 sm:py-12">
           <div className="flex flex-col gap-8 lg:flex-row lg:items-center lg:justify-between">
             <div className="max-w-3xl text-center lg:text-left">
               <p className="text-sm font-semibold text-blue-100">
